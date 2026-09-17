3929efc46d9b62ee8964019c62dd46e6b58684ee
sennqq <topor4ik02@gmail.com>
2026-09-17 05:16:43 +0300

    Script google loading optimization.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,10 @@ import {Rubik} from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
 import {MicrosoftClarity} from "@/components/Clarity";
-import {GoogleAnalytics} from "@next/third-parties/google";
+// import {GoogleAnalytics} from "@next/third-parties/google";
 import {getLocale} from "next-intl/server";
 import {GoogleTagManager} from "@next/third-parties/google";
-import Script from "next/script";
+// import Script from "next/script";
 
 const rubikMono = Rubik({
     variable: "--font-rubik",
@@ -31,26 +31,26 @@ export default async function RootLayout({
             lang={locale}
             className={`${rubikMono.variable} ${getVoip.variable} h-full antialiased`}
         >
-        <head>
-            <Script
-                src="https://www.googletagmanager.com/gtag/js?id=AW-18403181154"
-                strategy="afterInteractive"
-            />
-            <Script id="google-ads-tag" strategy="afterInteractive">
-                {`
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', 'AW-18403181154');
-                `}
-            </Script>
-        </head>
+        {/*<head>*/}
+        {/*    <Script*/}
+        {/*        src="https://www.googletagmanager.com/gtag/js?id=AW-18403181154"*/}
+        {/*        strategy="afterInteractive"*/}
+        {/*    />*/}
+        {/*    <Script id="google-ads-tag" strategy="afterInteractive">*/}
+        {/*        {`*/}
+        {/*          window.dataLayer = window.dataLayer || [];*/}
+        {/*          function gtag(){dataLayer.push(arguments);}*/}
+        {/*          gtag('js', new Date());*/}
+        {/*          gtag('config', 'AW-18403181154');*/}
+        {/*        `}*/}
+        {/*    </Script>*/}
+        {/*</head>*/}
         <body className="min-h-full flex flex-col font-rubik bg-[rgba(230,230,230,1)]">
         {children}
         <MicrosoftClarity/>
-        {process.env.NEXT_PUBLIC_GA_ID && (
-            <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID}/>
-        )}
+        {/*{process.env.NEXT_PUBLIC_GA_ID && (*/}
+        {/*    <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID}/>*/}
+        {/*)}*/}
         {process.env.NEXT_PUBLIC_GTM_ID && (
             <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID}/>
         )}
